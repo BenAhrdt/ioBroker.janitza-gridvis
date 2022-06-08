7712f982b4cb32f9f43ae8bc79d9b2da09b01baa
BenAhrdt <bsahrdt@gmail.com>
2022-06-08 16:07:43 +0200

    require time removed

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-const { time } = require("console");
 const axios = require("axios").default;
 const schedule = require("node-schedule");
 
